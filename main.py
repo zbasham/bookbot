c6e0422f4baceaa5f861e8c37ab038204f2d50a2
zbasham <zbasham916@gmail.com>
2023-12-31 15:49:17 -0800

    Completed bookbot tutorial project

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document.")
+    chars_dict = get_chars_dict(text)
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{num_words} words found in the document")
+    chars_list = list(chars_dict)
+    for letter in range(len(chars_list)):
+        if chars_list[letter].isalpha() == True:
+            print(f"The {chars_list[letter]} character was found {chars_dict[chars_list[letter]]}")
+    print("--- END REPORT ---")
 
 
 def get_book_text(book_path):
@@ -14,9 +21,17 @@ def get_num_words(text):
     words = text.split()
     return len(words)
 
-def count_letters(text)
-    lower_text = text.lower()
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
     
+
 
 
 
