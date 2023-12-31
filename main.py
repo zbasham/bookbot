28db2218765322e79c22d1b0ade4f4a94ed1228b
zbasham <zbasham916@gmail.com>
2023-12-30 17:45:20 -0800

    updated git ignore and main.py with text and word count

---
+++ main.py
@@ -0,0 +1,22 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document.")
+
+
+def get_book_text(book_path):
+    with open(book_path) as f:
+        file_contents = f.read()
+    return file_contents
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def count_letters(text)
+### Continue here
+
+
+
+main()
