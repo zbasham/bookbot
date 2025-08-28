2aa0bae825ec9efd1d3fcbeee8e2f3df23df181a
zbasham <zbasham916@gmail.com>
2025-08-27 21:52:12 -0700

    bookbot finished.. for now

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
-
+import sys
 
 def main():
-    book = get_book_text("books/frankenstein.txt")
+    try:
+        book = get_book_text(sys.argv[1])
+    except Exception as e:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
     num_words = word_count(book)
     characters = character_count(book)
-    print(f"{num_words} words found in the document")
-    print(characters)
+    
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {sys.argv[1]}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for i in sorted_characters(characters):
+        for key in i:
+            if key.isalpha() == True:
+                print(f"{key}: {i[key]}")
+            else:
+                pass
+    print("============= END ===============")
+
 
 
 # Gets the text from a specified book
@@ -15,8 +31,16 @@ def get_book_text(file_path):
     return text
 
 # Accepts a string 
-# Uses the text from the book specified in get_book_text to give a word count.
+# Uses the text from the book specified in get_book_text to give a word count
 from stats import word_count
 
+# Accepts a string
+# Creates a dictionary of every character in the book
 from stats import character_count
+
+# Accepts a dictionary of key=int(value) pairs
+# Returns a list of dictionaries of the characters in the book
+# The characters are sorted from most used to least used
+from stats import sorted_characters
+
 main()
--- stats.py
+++ stats.py
@@ -6,7 +6,7 @@ def word_count(text):
     total_words = len(list_of_words)
     return total_words
 
-# sample = "Please! Count my characters! :)"
+sample = "Please! Count my characters! :)"
 
 def character_count(text):
     characters = {}
@@ -17,4 +17,20 @@ def character_count(text):
         else:
             characters[i] += 1
     return characters
-            
+
+# Sorts a dictionary into new, single key:value dictionaries from greatest to smallest
+
+def sorted_characters(char_dict):
+    list_of_dicts = []
+    for i in char_dict:
+        small_dict = {}
+        small_dict[i] = char_dict[i]
+        list_of_dicts.append(small_dict)
+        list_of_dicts.sort(reverse = True, 
+            key = lambda each_dict: list(each_dict.values())[0]
+            )
+    return list_of_dicts
+
+
+#Test
+sorted_characters(character_count(sample))
